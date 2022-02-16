462e01157fd69d0a1f476427532f1499a567977c
Feruzteame <teamefitwi@gmail.com>
2022-02-16 14:31:48 +0100

    made currency change

--- index.php
+++ index.php
@@ -1,9 +1,70 @@
+
 <?php
- var_dump( $_GET );
+// value of different currency
+$ero = 1;
+$dollar = 0.88;
+$rand = 0.058;
+$rupee = 0.012;
+$AUD = 0.63;
+$pound = 0.056;
+
+// value of the input
+$value = $_GET["currency"];
+$place = $_GET["place"];
+$result = "See Your Change Here.";
+
+// checking of currency
+switch ($place) {
+    case "USA":
+         $result =  "You have ". $dollar * $value ."€" . " when your money change to EURO currency";
+        break;
+    case "Europe":
+        $result =   "You have ". $ero * $value ."€" . " when your money change to EURO currency";
+        break;
+    case "South Africa":
+        $result =   "You have ". $rand * $value ."€" . " when your money change to EURO currency";
+        break;
+    case "India":
+        $result =   "You have ". $rupee * $value ."€" . " when your money change to EURO currency";
+        break;
+    case "Australia":
+        $result =   "You have ". $AUD * $value ."€" . " when your money change to EURO currency";
+        break;
+    case "Egypt":
+        $result =   "You have ". $pound * $value ."€" . " when your money change to EURO currency";
+        break;
+    case "" && $value == " ":
+        echo "Please choose the place !";
+}
 ?>
 
-<form action="" method="get">
-    <label for="place">First name:</label><br>
-    <input type="text" name="place" id="place"><br>
-    <input type="submit" value="Submit">
-</form>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" name="description" content="This is my personal web-site">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Contact Me</title>
+        <link rel="stylesheet" href="style.css">
+   </head>
+      <body>
+        <form action="" method="get">
+            <input type="radio" id="place" name="place" value="USA"> USA
+            <input type="radio" id="place" name="place" value="Europe"> Europe
+            <input type="radio" id="place" name="place" value="South Africa"> South Africa
+            <input type="radio" id="place" name="place" value="India"> India
+            <input type="radio" id="place" name="place" value="Australia"> Australia
+            <input type="radio" id="place" name="place" value="Egypt"> Egypt<br>
+            <label for="currency" id="label" >currency Change</label><br><br>
+            <input type="number"  name="currency" id="currency"><br>
+            <input type="submit" value="Submit" id="submit">
+        </form>
+      <div id="result">
+          <?php echo $result ?>
+      </div>
+    </body>
+</html>
+
+
+
+
+
