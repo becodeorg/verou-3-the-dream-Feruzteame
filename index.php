70a1bf2a027d01c33d32b9614b39417b11076332
Feruzteame <teamefitwi@gmail.com>
2022-02-16 10:56:14 +0100

    create a form

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+ var_dump( $_GET );
+?>
+
+<form action="" method="get">
+    <label for="place">First name:</label><br>
+    <input type="text" name="place" id="place"><br>
+    <input type="submit" value="Submit">
+</form>
